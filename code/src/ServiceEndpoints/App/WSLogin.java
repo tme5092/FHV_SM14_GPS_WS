78865e08ebce552337f6229eb515329373df510e
tme5092 <tobias.metzler@students.fhv.at>
2015-10-17 16:05:43 +0200

    before reverting

--- code/src/ServiceEndpoints/App/WSLogin.java
+++ code/src/ServiceEndpoints/App/WSLogin.java
@@ -1,19 +1,45 @@
 package ServiceEndpoints.App;
 
-import javax.ws.rs.GET;
-import java.util.Calendar;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Author: Tobias
  * Date: 17.10.2015
  * Time: 11:40
  */
-@javax.ws.rs.Path("/Login")
+@javax.ws.rs.Path("/login")
 public class WSLogin {
 
     @GET
-    public String getServerTime() {
-        return Calendar.getInstance().toString();
+    @Path("/username/{usr}/password/{pw}/deviceid/{devid}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String login(@DefaultValue("") @PathParam("usr") String usr,@DefaultValue("") @PathParam("pw") String pw,@DefaultValue("") @PathParam("devid") String deviceID) {
+
+        /**
+         * Map containing Login/Pw
+         */
+        Map<String,String> userDefs = new HashMap<>();
+        userDefs.put("phe","phe");
+
+        JSONObject rs = new JSONObject();
+
+        boolean isSuccessfull = userDefs.containsKey(usr);
+        if ( isSuccessfull ) {
+            isSuccessfull = userDefs.get(usr).equals(pw);
+        }
+
+        try {
+            rs.append("success",isSuccessfull);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return rs.toString();
     }
 
 }
--- code/src/ServiceEndpoints/App/WSUserExists.java
+++ code/src/ServiceEndpoints/App/WSUserExists.java
@@ -4,7 +4,6 @@ import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.jvnet.jax_ws_commons.json.JSONBindingID;
 
-import javax.jws.WebParam;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.xml.ws.BindingType;
@@ -15,15 +14,9 @@ import java.util.HashSet;
  * Date: 17.10.2015
  * Time: 13:32
  */
-@Path("userExists")
+@Path("/userExists")
 @BindingType(JSONBindingID.JSON_BINDING)
 public class WSUserExists {
-
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String check(@WebParam(name="username")String username) {
-        return "Hello World: "+ username;
-    }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
